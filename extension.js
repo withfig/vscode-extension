1a9d9c7bbca13bf2f54ba3c61f4d735372cc55c1
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2021-06-09 05:42:18 -0700

    Remove unneeded comments

--- extension.js
+++ extension.js
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const cp = require('child_process')
 
@@ -11,28 +9,7 @@ let runCommand = function (command) {
 	});
 }
 
-// let figlog = function (vscode, console) {
-// 	console.log(vscode.window.activeTerminal, vscode.window.activeTextEditor, vscode.context)
-// 	console.log(vscode.window.activeTerminal.name)
-// 		(async () => {
-// 			let pid = await vscode.window.activeTerminal.processId
-// 			console.log(pid)
-// 		})()
-// }
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-
 function activate(context) {
-	// var next = vscode.window.terminals.length
-	// var uniqueTerminalIDs = vscode.window.terminals.reduce((elm, acc, idx) => {
-	// 	acc[elm] = idx
-	// 	return acc
-	// })
 	try {
 		console.log("Terminals: " + vscode.window.terminals.length);
 
@@ -47,7 +24,6 @@ function activate(context) {
 			console.log("Fig integration: " + e)
 		}
 
-		// vscode.window.onDidOpenTerminal
 		vscode.window.onDidChangeWindowState(event => {
 			console.log("onDidChangeWindowState", event)
 			console.log(vscode.window.activeTerminal, vscode.window.activeTextEditor, vscode.context)
@@ -70,18 +46,6 @@ function activate(context) {
 					runCommand('fig bg:vscode code:' + processId)
 				}
 			})
-
-			// var uid = uniqueTerminalIDs[terminal]
-			// console.log("uid:", uid)
-			// if (!uid) {
-			// 	uid = next++
-			// 	console.log(`Creating new UID (${uid}) for terminal ${terminal.name}`)
-			// 	uniqueTerminalIDs[terminal] = uid
-			// }
-
-			// console.log(uniqueTerminalIDs)
-
-
 		});
 
 		vscode.window.onDidCloseTerminal(terminal => {
@@ -93,23 +57,15 @@ function activate(context) {
 					runCommand('fig bg:vscode code:' + processId)
 				}
 			})
-			// let uid = uniqueTerminalIDs[terminal]
-			// console.log(`Removing UID (${uid}) for terminal ${terminal.name}`)
-
-			// delete uniqueTerminalIDs[terminal]
-			// console.log(uniqueTerminalIDs)
 		});
 
-
-
-		// vscode.window.onDidChangeActiveTerminal
 		vscode.window.onDidChangeActiveTerminal(terminal => {
 			console.log(`Active terminal changed, name=${terminal ? terminal.name : 'undefined'}`);
 			terminal.processId.then((processId) => {
 				if (processId) {
 					runCommand('fig bg:vscode code:' + processId)
 				}
-			})		//let pid = vscode.window.activeTerminal.processId
+			})
 			console.log(vscode.window.activeTerminal, vscode.window.terminals, vscode.window.terminals.indexOf(vscode.window.activeTerminal))
 		});
 
@@ -125,13 +81,9 @@ function activate(context) {
 	} catch (e) {
 		console.log("Fig integration: " + e)
 	}
-	// setInterval(() => {
-	// 	console.log(vscode.window.activeTerminal, vscode.window.activeTextEditor, vscode.context)
-	// }, 1000)
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() { }
 
 module.exports = {
